bd25fd717728a364350d152e83fe27953c9b8446
aliagasiyev <agasiyevali2@gmail.com>
2025-03-15 18:54:10 +0400

    in main

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Inside my app");
         System.out.println("Something else");
+        System.out.println("Helli ");
     }
 }
